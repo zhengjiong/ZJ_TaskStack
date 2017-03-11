cb626b178ca0d1ea8d4f9964e87189bb70bedc3d
zhengjiong <zhengjiong2006@gmail.com>
2017-03-11 18:01:25 +0800

    update manifest

--- app/src/main/java/com/zj/example/taskstack/MainActivity.java
+++ app/src/main/java/com/zj/example/taskstack/MainActivity.java
@@ -25,6 +25,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AActivity.class));
             }
         });
+
+
+        if (getIntent() != null) {
+            System.out.println("-_-_-_-_-_-_-_-_-_-_-_ZJ_TaskStack Start-_-_-_-_-_-_-_-_-_-_-_-_");
+            System.out.println("getAction=" + getIntent().getAction());
+            if (getIntent().getCategories() != null) {
+                for (String str : getIntent().getCategories()) {
+                    System.out.println("getCategories=" + str);
+                }
+            }
+            if (getIntent().getData() != null) {
+                System.out.println("schema=" + getIntent().getData().getScheme());
+                System.out.println("host=" + getIntent().getData().getHost());
+                System.out.println("path=" + getIntent().getData().getPath());
+                System.out.println("query=" + getIntent().getData().getQuery());
+            }
+            System.out.println("getDataString=" + getIntent().getDataString());
+            System.out.println("getComponent=" + getIntent().getComponent());
+            System.out.println("-_-_-_-_-_-_-_-_-_-_-_ZJ_TaskStack End-_-_-_-_-_-_-_-_-_-_-_-_");
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        System.out.println("onNewIntent");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("onDestroy");
         findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
